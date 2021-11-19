54b75bceb0566823a7b699f28189fa30310bf5a6
sathibabunaidu58 <sathibabunaidu58@gmail.com>
2021-11-19 17:52:03 +0530

    main

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%c&9w*b-xc5jvz-=9=1c_w8kfa=d!)bn&jtw3c3efogbz&ep$i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,10 +120,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
-STATICFILES_DIRS = [ BASE_DIR / 'static']
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/media/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+if DEBUG:
+    STATICFILES_DIRS = [ BASE_DIR / 'static']
+else:
+    STATIC_ROOT = [BASE_DIR / 'static']
+
+
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- blogproject/urls.py
+++ blogproject/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
 from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
     
